9f56c9d61189f784f69b4254e396f82b41b18aa7
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-04-25 23:19:49 +0900

    Lab7. Moore Machine

--- Lab7. FSM/fsm/fsm_moore.v
+++ Lab7. FSM/fsm/fsm_moore.v
@@ -1,32 +1,33 @@
-module fsm_moore (y, x, clock);
+module fsm_moore (Y, X, clock);
     input clock;
-    input x;
-    output y;
+    input X;
+    output Y;
     
     reg       state_master;
     reg [1:0] state_slave;
-    reg       y;
-    
-    initial begin
-        state_master = 0;
-        state_slave  = 0;
-    end
-    
+    reg       Y;
+
     always @(posedge clock) begin
-        if (state_master == x) 
-            $display("x=%b sm=%b", x, state_master);
-            if (state_slave < 2'b11) state_slave = state_slave + 1'b1;
-        else begin
-            state_master <= ~state_master;
-            state_slave  <= 0;
+        // next state logic
+        if (state_master == X) begin
+            $display("[MR] master state matches sm=%b, ss=%b, x=%b", state_master, state_slave, X);
+            if (state_slave != 2'b11) begin // when state less than 2'b11
+                state_slave = state_slave + 1;
+                $display("[MR] >> slave state updated ss=%b", state_slave);
+            end
         end
-
+        else begin
+            $display("[MR] state mismatch, update to %b, reset ss to 0", X);
+            
+            state_master = X;
+            state_slave  = 0;
+        end
+        
+        // output logic
         case (state_slave)
-            2'b00, 2'b01, 2'b10:   y = 0;
-            2'b11:                 y = 1;
-            default:               y = 1'bx;
+            2'b00, 2'b01, 2'b10:   Y = 0;
+            2'b11:                 Y = 1;
+            default:               Y = 1'bx;
         endcase
-
-        $display("x=%b y=%b sm=%b ss=%b", x, y, state_master, state_slave);
     end
 endmodule
