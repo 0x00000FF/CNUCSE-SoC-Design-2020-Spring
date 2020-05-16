b2b8f9a7ae7a081e658effbf451ae6ee39a3df47
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-17 08:10:56 +0900

    [check]type change: reg[15:0] -> integer

--- Lab8. KSA/ksa_16b/ksa_16b_tb.v
+++ Lab8. KSA/ksa_16b/ksa_16b_tb.v
@@ -7,18 +7,17 @@ module ksa_16b_tb;
     wire cout;
     wire [15:0] sum;
     
-    reg  [15:0] check;
-    integer     i, j;
+    integer     check, i, j;
     integer     num_correct, num_wrong;
     
     ksa_16b_top ksa(.cout(cout), .sum(sum), .a(a), .b(b), .cin(cin));
     
     initial begin
         num_correct = 0; num_wrong = 0;
-        
-        $display("initialized check parameters to 0");
+        $display("=== test begin ===", num_correct, num_wrong);
+
         // test for 16bit max (65535)
-        for (i = 0; i < 65536; i = i + 1) begin
+        for (i = 25452; i < 65536; i = i + 1) begin
             a = i;
             for (j = 0; j < 65536; j = j + 1) begin
                 b = j;
@@ -26,10 +25,14 @@ module ksa_16b_tb;
                 
                 check = a + b + cin;
                 #10;
-                if ({cout, sum} == check)
+                if ({cout, sum} == check) begin
+                    $display("[%d, %d] test case passed: %b", a, b, {cout, sum});
                     num_correct = num_correct + 1;
-                else
+                end
+                else begin
+                    $display("[%d, %d] test case failed: %b (expected %b)", a, b, {cout, sum}, check);
                     num_wrong   = num_wrong + 1;
+                end
             end
         end
         
