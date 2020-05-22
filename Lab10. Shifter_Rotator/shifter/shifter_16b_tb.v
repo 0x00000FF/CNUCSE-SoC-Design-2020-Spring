7bb86c0b1f9ef3080608ba257a561c6c98214feb
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-22 21:16:11 +0900

    Lab10: Add testcases for Rotation

--- Lab10. Shifter_Rotator/shifter/shifter_16b_tb.v
+++ Lab10. Shifter_Rotator/shifter/shifter_16b_tb.v
@@ -3,18 +3,18 @@
 module shifter_16b_tb;
     reg  [15:0] x, check;
     reg  [3:0]  sel;
-    reg         dir;
+    reg         dir, rot;
     
     wire  [15:0] y;
     
     integer    i, j;
     integer    check_correct, check_wrong;
     
-    shifter_16b_top  sh(.out(y), .x(x), .shift(sel), .dir(dir));
+    shifter_16b_top  sh(.out(y), .x(x), .shift(sel), .dir(dir), .rot(rot));
     
     initial begin
         x = 1;
-        sel = 0; dir = 0;
+        sel = 0; dir = 0; rot = 0; // no rotation; shift mode
         
         check_correct = 0; check_wrong = 0;
     
@@ -50,7 +50,35 @@ module shifter_16b_tb;
                 end
             end
             $display("CASE:   (x = %b), SHR 0 to 15 COMPLETE\n", x);
-            
+
+            dir = 0; rot = 0; // rotate left
+            for (j = 0; j < 16; j = j+1) begin
+                sel   = j;
+                check = { check[14-j:0], check[15:15-j] };
+                 
+                #10
+                if (y == check) check_correct = check_correct + 1;
+                else begin
+                    $display("TEST CASE MISMATCH: [%b R>> %d] %b (EXPECTED %b)", x, sel, y, check);
+                    check_wrong   = check_wrong + 1;
+                end
+            end
+            $display("CASE:   (x = %b), ROTL 0 to 15 COMPLETE\n", x);
+
+            dir = 0; rot = 0; // rotate right
+            for (j = 0; j < 16; j = j+1) begin
+                sel   = j;
+                check = { check[j-1:0], check[15:0+j] };
+                 
+                #10
+                if (y == check) check_correct = check_correct + 1;
+                else begin
+                    $display("TEST CASE MISMATCH: [%b R>> %d] %b (EXPECTED %b)", x, sel, y, check);
+                    check_wrong   = check_wrong + 1;
+                end
+            end
+            $display("CASE:   (x = %b), ROTR 0 to 15 COMPLETE\n", x);
+
             $display("CORRECT: %d, WRONG: %d SO FAR\n==========================================", 
                          check_correct, check_wrong);
         end
