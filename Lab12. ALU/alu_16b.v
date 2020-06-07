10b7af23e8b98b373933012ac6060ec1329c93a4
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-06-07 12:16:37 +0900

    Lab12: top module skeleton

---
+++ Lab12. ALU/alu_16b.v
@@ -0,0 +1,11 @@
+module alu_16v(cc, result, valA, valB, op, sub, lr, arlo);
+    input [15:0]  valA, valB;
+    input [2:0]   op;
+    input         sub, lr, arlo;
+
+    output [3:0]  cc;
+    output [15:0] result;
+
+
+
+endmodule
