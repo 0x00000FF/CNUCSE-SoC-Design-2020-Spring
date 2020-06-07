097a494e9a891fb542fa25dc8bc1143a3bc4f505
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-06-07 18:11:38 +0900

    Lab11. Complete Testbench

--- Lab11. Register_File/reg_file/RegFile16B_tb.v
+++ Lab11. Register_File/reg_file/RegFile16B_tb.v
@@ -1,11 +1,39 @@
 `timescale 1ns/100ps
 
+`define WRITE(addr, data)           \
+        #10                         \
+        writeEnable = 1;            \
+        writeAddr   = addr;         \
+        writeData   = data;         \
+        #10                         \
+        writeEnable = 0;            \
+        writeCount = writeCount + 1; 
+
+
+`define READ(addrA, addrB, expA, expB)                                                        \ 
+        #10                                                                                   \
+        readAddrA = addrA;                                                                    \
+        readAddrB = addrB;                                                                    \
+        #10                                                                                   \
+        if (readDataA == expA) readAMatchCount <= readAMatchCount + 1;                         \
+        else readAMismatchCount <= readAMismatchCount + 1;                                     \
+        if (readDataB == expB) readBMatchCount <= readBMatchCount + 1;                         \
+        else readBMismatchCount <= readBMismatchCount + 1;           
+           
+
+           
+
 module RegFile16B_tb;
     reg           CLK, nRST, writeEnable;
     reg  [3:0]    readAddrA, readAddrB, writeAddr;
     reg  [15:0]   writeData;
     
     wire [15:0]   readDataA, readDataB;
+
+    
+    integer     i, j,
+                writeCount, readAMatchCount, readAMismatchCount,
+                readBMatchCount, readBMismatchCount;
     
 
     RegFile16B regFile(.readDataA(readDataA), .readDataB(readDataB), 
@@ -22,6 +50,9 @@ module RegFile16B_tb;
         nRST = 1; writeEnable = 0;
         readAddrA = 4'b0000; readAddrB = 4'b0000; writeAddr = 4'b0000;
         writeData = 0;
+
+        writeCount = 0; readAMatchCount = 0; readAMismatchCount = 0;
+        readBMatchCount = 0; readBMismatchCount = 0;
         
         #5;
         nRST = 0; // reset system
@@ -29,18 +60,33 @@ module RegFile16B_tb;
         nRST = 1;
         
         #5
-        writeEnable = 1;
-        writeAddr   = 4'b0000;
-        writeData   = 16'b0000000000001000;
+
+        // reset test
+        $display(">>> [TEST] RESET");
+        for (i = 0; i < 16; i = i + 1) begin
+            `READ(i, i, 16'b0000000000000000, 16'b0000000000000000);
+        end
+
+        // write test
+        $display(">>> [TEST] WRITE AND READ FOR EACH REGISTER");
+        for (i = 0; i < 16; i = i + 1) begin
+            $display(">>>>>>>>> Proceeding to %d/16", i + 1);
+            for (j = 0; j <= 16'hffff; j = j + 1) begin 
+                `WRITE(i, j);
+                `READ(i, i, j, j);
+            end 
+        end
+
+        $display("=== TEST RESULT =====================");
+        $display("Total Write | %d", writeCount);
+        $display("=====================================");
+        $display("Match ReadA | %d", readAMatchCount);
+        $display("Wrong ReadA | %d", readAMismatchCount);
+        $display("=====================================");
+        $display("Match ReadB | %d", readBMatchCount);
+        $display("Wrong ReadB | %d", readBMismatchCount);
+        $display("=====================================");
         
-        #10
-        writeEnable = 0;
-        readAddrA   = 4'b0000;
-        readAddrB   = 4'b0001;
-        
-        #10
-        readAddrA   = 4'b0001;
-        readAddrB   = 4'b0000;
-        
+        #999999999999;
     end
 endmodule
